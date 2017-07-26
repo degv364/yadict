d406ac9a50f7c9f1b72951049318cdf39761f660
daniel garcia vaglio <degv364@gmail.com>
2017-07-25 22:45:18 -0600

    Add option buttons

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -14,7 +14,7 @@ const version    = document.getElementById('version')
 
 const nav_menu   = document.getElementById('nav_menu');
 const enunciado  = document.getElementById('enunciado')
-const answers    = document.getElementById('respuestas')
+const options    = document.getElementById('options')
 const result     = document.getElementById('resultado')
 const search_box = document.getElementById('search')
 
@@ -29,25 +29,49 @@ const nextBtn   = document.getElementById('next')
 //hide everything at the beguinning
 nav_menu.style.display   = 'none';
 enunciado.style.display  = 'none';
-answers.style.display    = 'none';
+options.style.display    = 'none';
 result.style.display     = 'none';
 search_box.style.display = 'none';
 
+//global state variables
+var global_exercises = false;
+var global_is_first = true;
+var global_exercise_index = false;
+var global_correct_option = false;
+
 
 //Functions------------------------------------------------------------------
-function display_enunciado(input_id){
+function display_enunciado(index){
+    global_exercise_index = index;
     var enunciado = document.getElementById('enunciado');
-    enunciado.innerHTML = "Este es el enunciado, hey! "+input_id;  
+    enunciado.innerHTML = global_exercises[index]["info"];  
 }
 
-function create_nav_button(input_id){
+function display_options(index){
+    global_exercise_index = index;
+    document.getElementById('option_a').innerHTML = global_exercises[index]["options"]["A"];
+    document.getElementById('option_b').innerHTML = global_exercises[index]["options"]["B"];
+    document.getElementById('option_c').innerHTML = global_exercises[index]["options"]["C"];
+    document.getElementById('option_d').innerHTML = global_exercises[index]["options"]["D"];
+    document.getElementById('option_e').innerHTML = global_exercises[index]["options"]["E"];
+    global_correct_option = global_exercises[index]["answer"];
+}
+
+function display_all(index){
+    	display_enunciado(index);
+	display_options(index);
+}
+
+
+function create_nav_button(index){
+    var input_id = global_exercises[index]["name"];
     var btn = document.createElement("button");
     btn.setAttribute("id", input_id);
     btn.setAttribute("class", "nav-button");
     var t = document.createTextNode(input_id);       
     btn.appendChild(t);
     btn.addEventListener('click', function(event){
-	display_enunciado(input_id);
+	display_all(index);
     });
     document.getElementById('exercises_list').appendChild(btn);                    
 }
@@ -59,26 +83,27 @@ startBtn.addEventListener('click', function (event) {
     nav_menu.style.display   = 'block';
     nav_menu.style.animation = "reFadeIn 5s forwards;";
     
-    document.getElementById('enunciado').style.display   = 'block';
-    document.getElementById('enunciado').style.animation = "reFadeIn 2s forwards;";
-    document.getElementById('respuestas').style.display  = 'block';
-    document.getElementById('resultado').style.display   = 'block';
-    document.getElementById('search').style.display      = 'block';
+    enunciado.style.display   = 'block';
+    enunciado.style.animation = "reFadeIn 2s forwards;";
+    options.style.display  = 'block';
+    result.style.display   = 'block';
+    search_box.style.display      = 'block';
 
-    var exercises_data = fs.readFileSync('data/exercises.json','utf8', function(err, data){
-	if (err) {
-	    return console.log(err);
+    if (global_is_first){
+	global_is_first = false;
+
+	var exercises_data = fs.readFileSync('data/exercises.json','utf8', function(err, data){
+	    if (err) {
+		return console.log(err);
+	    }
+	    return data
+	});
+	
+	global_exercises = JSON.parse(exercises_data); 
+	
+	for (index = 0; index < global_exercises.length; index++) {
+	    create_nav_button(index);
 	}
-	return data
-    });
-
-    var exercises = JSON.parse(exercises_data); 
-    console.log(exercises);
-
-    console.log(exercises.length.toString());
-
-    for (i = 0; i < exercises.length; i++) {
-	create_nav_button(exercises[i]["name"]);
     }
     
 })
@@ -96,7 +121,7 @@ codeBtn.addEventListener('click', function (event) {
 quitBtn.addEventListener('click', function(event) {
     nav_menu.style.display   = 'none';
     enunciado.style.display  = 'none';
-    answers.style.display    = 'none';
+    options.style.display    = 'none';
     result.style.display     = 'none';
     search_box.style.display = 'none';
 

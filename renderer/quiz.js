df7fbe5b16eb8e20cc0b29540864eb028e0f290b
daniel garcia vaglio <degv364@gmail.com>
2017-11-17 14:47:42 -0600

    Add timer

--- renderer/quiz.js
+++ renderer/quiz.js
@@ -176,3 +176,35 @@ beginQuizBtn.addEventListener('click', function(event){
     quiz_initialized = true;
     beginQuizBtn.style.display = 'none';
 })
+
+/// TIMER--------------------------------------------------------------------------
+
+// Set the date we're counting down to
+var countDownDate =(new Date().getTime())+ 5*60*1000; //5 min
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("CountDown").innerHTML = "Tiempo restante:<br> "
+  + minutes + " : " + seconds ;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+      document.getElementById("CountDown").innerHTML = "EXPIRED";
+      end_quiz();
+  }
+}, 1000);
